81f7b628fe9b67370c274e68f7e88eb76f1d59f6
Sophieynw <167501609+Sophieynw@users.noreply.github.com>
2025-01-11 19:26:28 -0500

    Added About, Decks, Home pages and routes in app.js

---
+++ frontend/src/pages/Decks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Decks() {
+    return (
+        <div>
+            <h1>Decks Page</h1>
+            <p>Manage your decks here.</p>
+        </div>
+    );
+}
+
+export default Decks;
