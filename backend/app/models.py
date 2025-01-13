67f079512136bfeab1d1b5ad631faee582fc5aab
Sophieynw <167501609+Sophieynw@users.noreply.github.com>
2025-01-13 00:36:26 -0500

    Fixed user authentification; login now takes user to /home

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,9 +1,13 @@
 from . import db
-from sqlalchemy.sql import func
 from flask_login import UserMixin
+from uuid import uuid4
+
+def get_uuid():
+    return uuid4().hex
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True, nullable=False)
+    __tablename__ = "users"
+    id = db.Column(db.String(32), primary_key=True, unique=True, default=get_uuid)
+    email = db.Column(db.String(345), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
     name = db.Column(db.String(150), nullable=False)
